2b4ca28e786631458e92d1b058faf2e76471b2f3
Aronpinkman <00149320@uca.edu.sv>
2025-06-13 07:56:20 -0600

    add:Videogameservice

--- labo3/src/main/java/com/example/labo3/Service/ServiceImplementation/VideogameServiceImpl.java
+++ labo3/src/main/java/com/example/labo3/Service/ServiceImplementation/VideogameServiceImpl.java
@@ -1,19 +1,21 @@
 package com.example.labo3.Service.ServiceImplementation;
 
 import com.example.labo3.Service.iVideogameService;
+import com.example.labo3.dto.request.VideoGameRequest;
+import com.example.labo3.dto.response.VideoGameResponse;
 
 public class VideogameServiceImpl implements iVideogameService {
     @Override
-    public VideojuegoResponseDTO createVideojuego(CreateVideojuegoDTO dto) throws Exception {
+    public VideoGameResponse createVideojuego(VideoGameRequest dto) throws Exception {
         return null;
     }
     @Override
-    public VideojuegoResponseDTO getVideojuegoById(Long idVideojuego) throws Exception {
+    public VideoGameResponse getVideojuegoById(Integer id) throws Exception {
         return null;
     }
 
     @Override
-    public VideojuegoResponseDTO getAllVideojuegos(Long idVideojuego) throws Exception {
+    public VideoGameResponse getAllVideojuegos(Integer id) throws Exception {
         return null;
     }
 }
