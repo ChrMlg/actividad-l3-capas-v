063c293830635fa2e4bb942b4eaea953656056a2
Andrea Peña <00075919@uca.edu.sv>
2025-06-13 08:23:04 -0600

    add: mapper file

---
+++ labo3/src/main/java/com/example/labo3/utils/mappers/VideoGameMapper.java
@@ -0,0 +1,33 @@
+package com.example.labo3.utils.mappers;
+
+import com.example.labo3.dto.response.VideoGameResponse;
+import com.example.labo3.entities.VideoGame;
+
+import java.util.List;
+
+public class VideoGameMapper {
+    public static VideoGame toEntityCreate(VideoGame videoGame) {
+        return VideoGame.builder()
+                .name(videoGame.getName())
+                .genre(videoGame.getGenre())
+                .releaseYear(videoGame.getReleaseYear())
+                .developer(videoGame.getDeveloper())
+                .build();
+    }
+
+    public static VideoGameResponse toDTO(VideoGame videoGame) {
+        return VideoGameResponse.builder()
+                .id(videoGame.getId())
+                .name(videoGame.getName())
+                .genre(videoGame.getGenre())
+                .releaseYear(videoGame.getReleaseYear())
+                .developer(videoGame.getDeveloper())
+                .build();
+    }
+
+    public static List<VideoGameResponse> toDTOList(List<VideoGame> videoGames) {
+        return videoGames.stream()
+                .map(VideoGameMapper::toDTO)
+                .toList();
+    }
+}
